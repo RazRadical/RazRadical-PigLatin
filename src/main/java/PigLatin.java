7ed980bf176b52f650fe6177bf6c32fe3cf92d1e
RazaDon Willrich <razradical93@gmail.com>
2025-04-18 07:28:02 +0000

    Completed Pig Latin.

--- src/main/java/PigLatin.java
+++ src/main/java/PigLatin.java
@@ -17,6 +17,11 @@ public class PigLatin {
      * @return the pig latin form of in.
      */
     public String returnPigLatin(String in){
-        return "";
+
+        String newString = in.substring(1, in.length());
+        newString += in.charAt(0);
+        newString += "ay";
+
+        return newString;
     }
 }
